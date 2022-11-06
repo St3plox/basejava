1f06a61be820f988b3496e140c94225fc55e35db
St3plox <76905680+St3plox@users.noreply.github.com>
2022-11-06 19:10:20 +0700

    Realized save and get methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -8,10 +10,30 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] != null) {
+                if (Objects.equals(storage[i].uuid, r.uuid)) {
+                    System.out.println("Ошибка. Элелемент с таким uuid уже существует");
+                    break;
+                }
+            }
+            if(storage[i] == null){
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        if (storage[0] == null) return null;
+        else {
+            int i = 0;
+            while (storage[i] != null) {
+                if (Objects.equals(storage[i].uuid, uuid)) return storage[i];
+                i++;
+            }
+            return  null;
+        }
     }
 
     void delete(String uuid) {
